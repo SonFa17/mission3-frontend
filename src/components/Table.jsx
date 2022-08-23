4b83abc0d517d459aa9967c8c38d27a8c675b285
Sonja Fantom <sonja.fantom@gmail.com>
2022-08-23 23:02:05 +1200

    commented out Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,20 @@
-const Table = ({ data }) => {
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <th>Search Result</th>
-          </tr>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.car_make}</td>
-              <td>{item.animal_scientific}</td>
-              <td>{item.car_model}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
+// const Table = ({ data }) => {
+//     return (
+//       <table>
+//         <tbody>
+//           <tr>
+//             <th>Search Result</th>
+//           </tr>
+//           {data.map((item) => (
+//             <tr key={item.id}>
+//               <td>{item.car_make}</td>
+//               <td>{item.animal_scientific}</td>
+//               <td>{item.car_model}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     );
+//   };
   
-  export default Table;
+//   export default Table;
