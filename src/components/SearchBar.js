1220ea94f04f669accbf7d7e0e9d987ba1926b86
Ayman Milhem <aymanmilhem@gmail.com>
2022-08-26 03:35:19 +1200

    make pressing the 'Enter' button equivalent to clicking on the 'Search' button, when in search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,56 +1,55 @@
-import React from 'react'
+import React from "react";
 import "./SearchBar.css";
 import { useState } from "react";
 
-
-
 const SearchBar = (props) => {
-    
-    const {onSearch} = props;
+  const { onSearch } = props;
 
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
+  const handleInput = (e) => {
+    const text = e.target.value;
+    setSearch(text);
+  };
 
+  const handleOnClick = () => {
+    onSearch(search);
+    const string = search;
+    const newString = string.replace(/[^\w\s]|_/g, " ");
+    console.log(newString);
+    setSearch(newString);
+    return newString;
+  };
 
-    const handleInput = (e) => {
-        const text = e.target.value
-        setSearch(text)
+  const handleEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleOnClick();
     }
+  };
+  return (
+    <>
+      <div className="search-box">
+        <div className="search-container">
+          <input
+            className="search-bar"
+            placeholder="Enter your question here"
+            type="text"
+            value={search}
+            onChange={handleInput}
+            onKeyPress={handleEnterKeyPress}
+          />
 
+          <button
+            onClick={handleOnClick}
+            className="search-bar-button"
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-    const handleOnClick = () => {
-            onSearch(search)
-            const string = search
-            const newString = string.replace(/[^\w\s]|_/g, " ")
-            console.log(newString)
-            setSearch(newString)
-            return newString;
-        }
-
-
-    return (
-        <>
-
-            <div className='search-box'>
-
-                <div className='search-container'>
-                    <input
-                        className='search-bar'
-                        placeholder="Enter your question here"
-                        type="text"
-                        value={search}
-                        onChange={handleInput}
-                    />
-
-                    <button onClick={handleOnClick} className="search-bar-button"
-                        type="button" >Search</button>
-                </div>
-            </div>
-        </>
-
-    )
-}
-
-
-export default SearchBar
+export default SearchBar;
