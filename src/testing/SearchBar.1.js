9a2aa9b8ca44ab42489e03cebb835aa43b200def
Ayman Milhem <aymanmilhem@gmail.com>
2022-08-26 05:35:02 +1200

    remove unnecessary folder

--- src/testing/SearchBar.1.js
+++
@@ -1,6 +0,0 @@
-const { handleOnClick } = require("../components/SearchBar");
-
-test('should trim "hello%world" and return "hello world"', () => { 
-    const text = handleOnClick('hello%world');
-    expect(text).toBe('hello world');
- })
